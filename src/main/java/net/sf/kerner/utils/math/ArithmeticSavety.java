6f7f8eda4891b066712dfc15ec668c009c749128
Kerner <alex.kerner.24@googlemail.com>
2010-11-22 11:58:06 +0100

    JavaDoc.

--- src/main/java/net/sf/kerner/utils/math/ArithmeticSavety.java
+++ src/main/java/net/sf/kerner/utils/math/ArithmeticSavety.java
@@ -1,7 +1,40 @@
+/**********************************************************************
+Copyright (c) 2009-2010 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.kerner.utils.math;
 
 import java.math.BigInteger;
 
+/**
+ * 
+ * Perform arithmetic operations with number over- and underflow check. 
+ * 
+ * <p>
+ * <b>Example:</b><br>
+ *
+ * </p>
+ * <p>
+ * <pre>
+ * TODO example
+ * </pre>
+ * </p>
+ *
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version 2010-11-22
+ *
+ */
 public class ArithmeticSavety {
 
 	public static final BigInteger BIG_MAX_INT = BigInteger
