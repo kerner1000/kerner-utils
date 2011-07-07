1a4e373012223c4716271cbdb840c8cb284370a7
Kerner <alex.kerner.24@googlemail.com>
2011-07-07 18:17:45 +0200

    [Feature] introduce Prefixable and Unit.

---
+++ src/main/java/net/sf/kerner/utils/math/Prefixable.java
@@ -0,0 +1,23 @@
+package net.sf.kerner.utils.math;
+
+public interface Prefixable {
+	
+	long toPicos(long units);
+	
+	long toNanos(long units);
+	
+	long toMicros(long units);
+	
+	long toMillis(long units);
+	
+	long toUnits(long units);
+	
+	long toKilos(long units);
+	
+	long toMegas(long units);
+	
+	long toGigas(long units);
+	
+	long toTeras(long units);
+
+}
