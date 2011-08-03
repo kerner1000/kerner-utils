13d62b18f83a0e3d9259d5ffcf8ff3e57fa1ad21
Kerner <alex.kerner.24@googlemail.com>
2011-08-03 11:58:15 +0200

    [Feature, Fix] Add Point#hashCode(), #equals(), #toString().

--- src/main/java/net/sf/kerner/utils/Point.java
+++ src/main/java/net/sf/kerner/utils/Point.java
@@ -103,4 +103,42 @@ public class Point {
 		return y;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Point)) {
+			return false;
+		}
+		Point other = (Point) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) {
+			return false;
+		}
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "[x=" + x + ",y=" + y + "]";
+	}
+
 }
