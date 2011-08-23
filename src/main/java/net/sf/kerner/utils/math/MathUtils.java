d7f35e058322cb021b27007c294b649c5ff4242f
Kerner <alex.kerner.24@googlemail.com>
2011-08-23 10:30:40 +0200

    [API-Break] Remove slope stuff.

--- src/main/java/net/sf/kerner/utils/math/MathUtils.java
+++ src/main/java/net/sf/kerner/utils/math/MathUtils.java
@@ -1,9 +1,5 @@
 package net.sf.kerner.utils.math;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import net.sf.kerner.utils.Utils;
 import net.sf.kerner.utils.counter.Counter;
 
 public class MathUtils {
@@ -35,78 +31,6 @@ public class MathUtils {
 		final double result = Math.round(number * factor) / factor;
 //		System.out.println("result: " + result);
 		return result;
-	}
-	
-	public static List<Integer> getPositionsOfExtremSlopes(List<? extends Point> points, double maxSlope, boolean absolute){
-		Utils.checkForNull(points);
-		if(maxSlope < 0)
-			throw new IllegalArgumentException("maxSlope must be >= 0");
-		
-		final List<Integer> result = new ArrayList<Integer>();
-		
-		final List<Double> slopes = getSlopes(points);
-		
-		for(int i = 0; i < slopes.size(); i++){
-			double slope = slopes.get(i);
-			if(absolute)
-				slope = Math.abs(slope);
-			
-//			System.out.println("slope " + slope);
-			
-			if(slope > maxSlope)
-				result.add(Integer.valueOf(i));
-		}
-		
-//		System.out.println("returning " + result);
-		
-		return result;
-	}
-	
-	public static List<Integer> getPositionsOfExtremSlopes(List<? extends Point> points, double maxSlope){
-		return getPositionsOfExtremSlopes(points, maxSlope, true);
-	}
-	
-	public static List<Double> getSlopes(List<? extends Point> points, boolean absolute){
-		Utils.checkForNull(points);
-		final List<Double> result = new ArrayList<Double>();
-		
-		if(points.size() < 1)
-			return result;
-		
-		Point last = points.iterator().next();
-		
-		for(int i = 1; i < points.size(); i++){
-			final Point p = points.get(i);
-			double slope = last.getSlope(p);
-			if(absolute)
-				slope = Math.abs(slope);
-			result.add(slope);
-			last = p;
-		}
-		
-//		System.out.println("returning " + result);
-		
-		return result;
-	}
-	
-	public static List<Double> getSlopes(List<? extends Point> points){
-		return getSlopes(points, false);
-	}
-	
-	public static double getAverageSlope(List<? extends Point> points, boolean absolute){
-		final List<Double> slopes = getSlopes(points, absolute);
-		final int size = slopes.size();
-		double result = 0;
-		
-		for(Double s : slopes){
-			result += s;
-		}
-		
-		return result / (double)size;
-	}
-	
-	public static double getAverageSlope(List<? extends Point> points){
-		return getAverageSlope(points, false);
 	}
 
 }
--- src/test/java/net/sf/kerner/utils/math/TestMathUtils.java
+++ src/test/java/net/sf/kerner/utils/math/TestMathUtils.java
@@ -3,22 +3,14 @@
  */
 package net.sf.kerner.utils.math;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.List;
-
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * 
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
- * @version 2011-04-13
+ * @version 2011-08-23
  *
  */
 public class TestMathUtils {
@@ -78,57 +70,5 @@ public class TestMathUtils {
 	public final void testRound06() {
 		assertEquals(10000000.0000, MathUtils.round(10000000.0000123, 4), 0);
 	}
-	
-	@Test
-	public final void testGetPositionsOfExtremSlopes01(){
-		final List<Point> points = Arrays.asList();
-		final List<Integer> out = MathUtils.getPositionsOfExtremSlopes(points, 1, true);
-		assertNotNull(out);
-	}
-	
-	@Test
-	public final void testGetPositionsOfExtremSlopes02(){
-		final List<Point> points = Arrays.asList();
-		final List<Integer> out = MathUtils.getPositionsOfExtremSlopes(points, 1, true);
-		assertTrue(out.isEmpty());
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public final void testGetPositionsOfExtremSlopes03(){
-		final List<Point> points = Arrays.asList();
-		MathUtils.getPositionsOfExtremSlopes(points, -1);
-	}
-	
-	@Test
-	public final void testGetPositionsOfExtremSlopes04(){
-		final List<Point> points = Arrays.asList(new Point(0,0), new Point(1,1));
-		final List<Integer> out = MathUtils.getPositionsOfExtremSlopes(points, 0, true);
-		assertArrayEquals(new Integer[]{0}, out.toArray());
-	}
-	
-	@Test
-	public final void testGetPositionsOfExtremSlopes05(){
-		final List<Point> points = Arrays.asList(new Point(0,0), new Point(1,1), new Point(0,-1));
-		final List<Integer> out = MathUtils.getPositionsOfExtremSlopes(points, 1, true);
-		assertArrayEquals(new Integer[]{1}, out.toArray());
-	}
-	
-	@Test
-	public final void testGetPositionsOfExtremSlopes06(){
-		final List<Point> points = Arrays.asList(new Point(0,0), new Point(1,1), new Point(0,2.5));
-		final List<Integer> out = MathUtils.getPositionsOfExtremSlopes(points, 1, true);
-		assertArrayEquals(new Integer[]{1}, out.toArray());
-	}
-	
-	@Test
-	public final void testGetAverageSlope01(){
-		final List<Point> points = Arrays.asList(new Point(0,0), new Point(1,1), new Point(2,2));
-		assertEquals(1, MathUtils.getAverageSlope(points, false), 0);
-	}
-	
-	@Test
-	public final void testGetAverageSlope02(){
-		final List<Point> points = Arrays.asList(new Point(0,0), new Point(1,1), new Point(2,2), new Point(2,2));
-		assertEquals(0.666, MathUtils.getAverageSlope(points, false), 0.001);
-	}
+
 }
