cec43af75fe8d4e542ef3a4dde377c4a9ad30edb
Kerner <alex.kerner.24@googlemail.com>
2011-08-29 09:15:45 +0200

    Cleanup.

--- src/main/java/net/sf/kerner/utils/math/MathUtils.java
+++ src/main/java/net/sf/kerner/utils/math/MathUtils.java
@@ -1,7 +1,5 @@
 package net.sf.kerner.utils.math;
 
-import java.util.Collection;
-
 import net.sf.kerner.utils.counter.Counter;
 
 public class MathUtils {
@@ -22,55 +20,47 @@ public class MathUtils {
 	public static double round(double number, int decimalPlace) {
 		int n = decimalPlace;
 		Counter c = new Counter();
-		while(n > 10){
+		while (n > 10) {
 			n = n / 10;
 			c.count();
 		}
-		
+
 		decimalPlace = decimalPlace + c.getCount();
-//		System.out.println("number: " + number + ", digits: " + decimalPlace);
+		// System.out.println("number: " + number + ", digits: " +
+		// decimalPlace);
 		final double factor = Math.pow(10, decimalPlace);
 		final double result = Math.round(number * factor) / factor;
-//		System.out.println("result: " + result);
+		// System.out.println("result: " + result);
 		return result;
 	}
-	
-	public static int max(Integer... values){
+
+	public static int max(Integer... values) {
 		int result = Integer.MIN_VALUE;
-		for(int i : values){
-			if(i > result)
-			result = i;
+		for (int i : values) {
+			if (i > result)
+				result = i;
 		}
 		return result;
 	}
-	
-	public static int min(Integer... values){
+
+	public static int min(Integer... values) {
 		int result = Integer.MAX_VALUE;
-		for(int i : values){
-			if(i < result)
-			result = i;
+		for (int i : values) {
+			if (i < result)
+				result = i;
 		}
 		return result;
 	}
 
-public static double average(Collection<Double> values){
-		return sum(values) / values.size();
-	}
-	
-	public static double sum(Collection<Double> values){
-		double result = 0;
-		for(double d : values){
-			result += d;
-		}
-		return result;
-	}
-	
-	public static double sum(double[] values){
-		double result = 0;
-		for(double d : values){
-			result += d;
-		}
-		return result;
+	public static double average(Double... values) {
+		return sum(values) / values.length;
 	}
 
+	public static double sum(Double... values) {
+		double result = 0;
+		for (double d : values) {
+			result += d;
+		}
+		return result;
+	}
 }
