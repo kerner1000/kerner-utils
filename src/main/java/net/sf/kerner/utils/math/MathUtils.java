0e167154c36fe5250bca55fe613463eb35a62b16
Kerner <alex.kerner.24@googlemail.com>
2011-10-25 09:46:37 +0200

    MathUtils: Add JavaDoc. round() does not use Counter anymore but simple int.

--- src/main/java/net/sf/kerner/utils/math/MathUtils.java
+++ src/main/java/net/sf/kerner/utils/math/MathUtils.java
@@ -1,10 +1,32 @@
+/**********************************************************************
+Copyright (c) 2009-2011 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.kerner.utils.math;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sf.kerner.utils.counter.Counter;
-
+/**
+ * 
+ * Provides static helper methods. 
+ * 
+ *
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version 2011-10-25
+ *
+ */
 public class MathUtils {
 
 	private MathUtils() {
@@ -22,18 +44,14 @@ public class MathUtils {
 	 */
 	public static double round(double number, int decimalPlace) {
 		int n = decimalPlace;
-		Counter c = new Counter();
+		int c = 0;
 		while (n > 10) {
 			n = n / 10;
-			c.count();
+			c ++;
 		}
-
-		decimalPlace = decimalPlace + c.getCount();
-		// System.out.println("number: " + number + ", digits: " +
-		// decimalPlace);
+		decimalPlace = decimalPlace + c;
 		final double factor = Math.pow(10, decimalPlace);
 		final double result = Math.round(number * factor) / factor;
-		// System.out.println("result: " + result);
 		return result;
 	}
 
