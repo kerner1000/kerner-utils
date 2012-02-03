6802d8cd4dab33f268b0dac7fd39e2fcf9ca6884
Kerner <alex.kerner.24@googlemail.com>
2012-02-03 16:39:25 +0100

    [Feature] Introduce MathUtils#median().

--- src/main/java/net/sf/kerner/utils/math/MathUtils.java
+++ src/main/java/net/sf/kerner/utils/math/MathUtils.java
@@ -15,6 +15,8 @@ limitations under the License.
 
 package net.sf.kerner.utils.math;
 
+import java.util.Arrays;
+
 import net.sf.kerner.utils.ArrayUtils;
 
 /**
@@ -74,7 +76,7 @@ public class MathUtils {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 
 	 * Get maximum of given values.
@@ -124,6 +126,23 @@ public class MathUtils {
 		if (values == null || values.length < 1)
 			throw new IllegalArgumentException();
 		return sum(values) / values.length;
+	}
+
+	public static double median(double... values) {
+		if (values == null || values.length < 1)
+			throw new IllegalArgumentException();
+		
+		// TODO necessary?
+		double[] b = new double[values.length];
+		System.arraycopy(values, 0, b, 0, b.length);
+		Arrays.sort(b);
+		
+		
+		if (values.length % 2 == 0) {
+			return (b[(b.length / 2) - 1] + b[b.length / 2]) / 2;
+		} else {
+			return b[b.length / 2];
+		}
 	}
 
 	/**
