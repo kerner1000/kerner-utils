b8f489cca0b267608d488dc0a11a836949ab81d7
Kerner <alex.kerner.24@googlemail.com>
2012-02-12 21:59:36 +0100

    JavaDoc.

--- src/main/java/net/sf/kerner/utils/math/MathUtils.java
+++ src/main/java/net/sf/kerner/utils/math/MathUtils.java
@@ -26,7 +26,7 @@ import net.sf.kerner.utils.ArrayUtils;
  * 
  * 
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
- * @version 2012-01-13
+ * @version 2012-02-12
  * 
  */
 public class MathUtils {
