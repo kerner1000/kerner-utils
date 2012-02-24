f5951186f5535e0039815747b534c4f316dc6c64
Kerner <alex.kerner.24@googlemail.com>
2012-02-24 17:20:56 +0100

    [Feature] Add PropertyNotFoundException.

---
+++ src/main/java/net/sf/kerner/utils/PropertyNotFoundException.java
@@ -0,0 +1,26 @@
+package net.sf.kerner.utils;
+
+public class PropertyNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = -4880504253227652735L;
+
+	public PropertyNotFoundException() {
+		
+	}
+
+	public PropertyNotFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		
+	}
+
+	public PropertyNotFoundException(String arg0) {
+		super(arg0);
+		
+	}
+
+	public PropertyNotFoundException(Throwable arg0) {
+		super(arg0);
+		
+	}
+	
+}
