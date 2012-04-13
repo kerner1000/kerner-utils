1a1bd3b459cc77fe06cd55e86e35a905dfe761c4
Kerner <alex.kerner.24@googlemail.com>
2012-04-13 18:23:37 +0200

    API-Break: Remove StringUtils#toStringList(). Use ListUtils instead.

--- src/main/java/net/sf/kerner/utils/StringUtils.java
+++ src/main/java/net/sf/kerner/utils/StringUtils.java
@@ -1,16 +1,60 @@
+/**********************************************************************
+Copyright (c) 2009-2012 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.kerner.utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * 
+ * Utility class for {@link String} related stuff.
+ * 
+ * <p>
+ * <b>Example:</b><br>
+ * 
+ * </p>
+ * <p>
+ * 
+ * <pre>
+ * TODO example
+ * </pre>
+ * 
+ * </p>
+ * 
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version Apr 13, 2012
+ * 
+ */
 public class StringUtils {
 
 	private StringUtils() {
 	}
 
+	/**
+	 * System dependent new line string.
+	 */
 	public final static String NEW_LINE_STRING = System.getProperty("line.separator");
 
+	/**
+	 * 
+	 * 
+	 * 
+	 * @return a random {@code String}
+	 */
 	public static String getRandomString() {
 		final String result = Long.toHexString(Double.doubleToLongBits(Math.random()));
 		return result;
@@ -18,12 +62,12 @@ public class StringUtils {
 
 	/**
 	 * 
-	 * Check if a string is null, empty or contains only whitespaces.
+	 * Check if a string is {@code null}, empty or contains only whitespaces.
 	 * 
 	 * @param string
-	 *            String to check
-	 * @return true, if this string is null, empty or contains only whitespaces;
-	 *         false otherwise
+	 *            {@code String} to check
+	 * @return true, if this {@code String} is {@code null}, empty or contains
+	 *         only whitespaces; false otherwise
 	 */
 	public static boolean emptyString(String string) {
 		if (string == null)
@@ -33,13 +77,5 @@ public class StringUtils {
 		if (string.matches("\\s"))
 			return true;
 		return false;
-	}
-
-	public static List<String> getStringList(Collection<?> list) {
-		final List<String> result = new ArrayList<String>();
-		for (Object o : list) {
-			result.add(o.toString());
-		}
-		return result;
 	}
 }
