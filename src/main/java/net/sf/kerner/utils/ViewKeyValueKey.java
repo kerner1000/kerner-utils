9ba206b52f8644d43bb27e347f9b1b153b5d40a5
Kerner <alex.kerner.24@googlemail.com>
2012-04-13 18:29:24 +0200

    API-Break: Rename.

--- src/main/java/net/sf/kerner/utils/ViewKeyValueKey.java
+++ src/main/java/net/sf/kerner/utils/ViewKeyValueKey.java
@@ -1,6 +1,6 @@
 package net.sf.kerner.utils;
 
-public class KeyValueViewKey<K> implements View<KeyValue<K, ?>, K> {
+public class ViewKeyValueKey<K> implements View<KeyValue<K, ?>, K> {
 
 	public K transform(KeyValue<K, ?> element) {
 		return element.getKey();
