eaf0d57a6ccd4abdea892e4bda14ab54c469306e
Kerner <alex.kerner.24@googlemail.com>
2012-04-25 11:45:04 +0200

    Add test.

--- src/test/java/net/sf/kerner/utils/KeyValueTest.java
+++ src/test/java/net/sf/kerner/utils/KeyValueTest.java
@@ -1,6 +1,18 @@
-/**
- * 
- */
+/**********************************************************************
+Copyright (c) 2009-2012 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.kerner.utils;
 
 import static org.junit.Assert.assertEquals;
@@ -17,7 +29,7 @@ import org.junit.Test;
 /**
  * 
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
- * @version 2011-01-13
+ * @version 2012-04-25
  * 
  */
 public class KeyValueTest {
@@ -50,129 +62,78 @@ public class KeyValueTest {
 	public void tearDown() throws Exception {
 	}
 
-	/**
-	 * Test method for {@link net.sf.kerner.utils.KeyValue#hashCode()}.
-	 */
 	@Test
 	public final void testHashCode() {
 		assertNotSame(key1.hashCode(), key2.hashCode());
 	}
 
-	/**
-	 * Test method for {@link net.sf.kerner.utils.KeyValue#hashCode()}.
-	 */
 	@Test
 	public final void testHashCode01() {
 		k2 = new KeyValue<String, String>(key1);
 		assertEquals(k1.hashCode(), k2.hashCode());
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#KeyValue(java.lang.Object, java.lang.Object)}
-	 * .
-	 */
 	@Test
 	public final void testKeyValueKV() {
 		k1 = new KeyValue<String, String>("key1", "value1");
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#KeyValue(java.lang.Object, java.lang.Object)}
-	 * .
-	 */
 	@Test
 	public final void testKeyValueKV01() {
 		k1 = new KeyValue<String, String>("key1", "value1");
 		assertEquals("key1", k1.getKey());
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#KeyValue(java.lang.Object, java.lang.Object)}
-	 * .
-	 */
 	@Test
 	public final void testKeyValueKV02() {
 		k1 = new KeyValue<String, String>("key1", "value");
 		assertEquals("value", k1.getValue());
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#KeyValue(java.lang.Object, java.lang.Object)}
-	 * .
-	 */
 	@Test(expected = NullPointerException.class)
 	public final void testKeyValueKV03() {
 		k1 = new KeyValue<String, String>(null, null);
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#KeyValue(java.lang.Object)}.
-	 */
 	@Test(expected = NullPointerException.class)
 	public final void testKeyValueK() {
 		k1 = new KeyValue<String, String>((KeyValue<String, String>) null);
 	}
+	
+	@Test(expected = NullPointerException.class)
+	public final void testKeyValueK01() {
+		k1 = new KeyValue<String, String>((String) null);
+	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#KeyValue(net.sf.kerner.utils.KeyValue)}
-	 * .
-	 */
 	@Test
 	public final void testKeyValueKeyValueOfKV() {
 		k1 = new KeyValue<String, String>(k2);
 		assertEquals(k2.getKey(), k1.getKey());
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#KeyValue(net.sf.kerner.utils.KeyValue)}
-	 * .
-	 */
 	@Test
 	public final void testKeyValueKeyValueOfKV01() {
 		k1 = new KeyValue<String, String>(k2);
 		assertEquals(k2.getValue(), k1.getValue());
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#equals(java.lang.Object)}.
-	 */
 	@Test
 	public final void testEqualsObject() {
 		k1 = new KeyValue<String, String>(k2);
 		assertEquals(k1, k2);
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#equals(java.lang.Object)}.
-	 */
 	@Test
 	public final void testEqualsObject01() {
 		assertEquals(k1, k1);
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#equals(java.lang.Object)}.
-	 */
 	@Test
 	public final void testEqualsObject02() {
 		k2 = new KeyValue<String, String>("key1", null);
 		assertEquals(k1, k2);
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#equals(java.lang.Object)}.
-	 */
 	@Test
 	public final void testEqualsObject03() {
 		k1 = new KeyValue<String, String>("key1", "value");
@@ -180,37 +141,23 @@ public class KeyValueTest {
 		assertEquals(k1, k2);
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#equals(java.lang.Object)}.
-	 */
 	@Test
 	public final void testEqualsObject04() {
 		assertNotSame(k1, k2);
 	}
 
-	/**
-	 * Test method for {@link net.sf.kerner.utils.KeyValue#getKey()}.
-	 */
 	@Test
 	@Ignore
 	public final void testGetKey() {
 		fail("Not yet implemented"); // TODO
 	}
 
-	/**
-	 * Test method for {@link net.sf.kerner.utils.KeyValue#getValue()}.
-	 */
 	@Test
 	@Ignore
 	public final void testGetValue() {
 		fail("Not yet implemented"); // TODO
 	}
 
-	/**
-	 * Test method for
-	 * {@link net.sf.kerner.utils.KeyValue#setValue(java.lang.Object)}.
-	 */
 	@Test
 	public final void testSetValue() {
 		k2.setValue("hans");
