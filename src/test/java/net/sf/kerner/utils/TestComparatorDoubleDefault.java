4b56090c4db068040b45942c00065f3079785236
Kerner <alex.kerner.24@googlemail.com>
2012-04-25 13:50:03 +0200

    Add tests.

---
+++ src/test/java/net/sf/kerner/utils/TestComparatorDoubleDefault.java
@@ -0,0 +1,47 @@
+package net.sf.kerner.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestComparatorDoubleDefault {
+	
+	private ComparatorDoubleDefault c;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		c = new ComparatorDoubleDefault();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testCompare01() {
+		assertTrue(0 > c.compare(Double.valueOf(1), Double.valueOf(2)));
+	}
+	
+	@Test
+	public final void testCompare02() {
+		assertTrue(0 < c.compare(Double.valueOf(3), Double.valueOf(0)));
+	}
+	
+	@Test
+	public final void testCompare03() {
+		assertTrue(0 == c.compare(Double.valueOf(3), Double.valueOf(3)));
+	}
+
+}
---
+++ src/test/java/net/sf/kerner/utils/TestComparatorIntegerDefault.java
@@ -0,0 +1,47 @@
+package net.sf.kerner.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestComparatorIntegerDefault {
+	
+	private ComparatorIntegerDefault c;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		c = new ComparatorIntegerDefault();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testCompare01() {
+		assertTrue(0 > c.compare(Integer.valueOf(1), Integer.valueOf(2)));
+	}
+	
+	@Test
+	public final void testCompare02() {
+		assertTrue(0 < c.compare(Integer.valueOf(3), Integer.valueOf(0)));
+	}
+	
+	@Test
+	public final void testCompare03() {
+		assertTrue(0 == c.compare(Integer.valueOf(3), Integer.valueOf(3)));
+	}
+
+}
