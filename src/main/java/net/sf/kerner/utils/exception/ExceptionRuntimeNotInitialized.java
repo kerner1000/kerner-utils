7cb7fa54bda46192d64d856c09629b201b4ee1a6
Kerner <alexanderkerner24@gmail.com>
2012-11-20 19:46:11 +0100

    Feature: Introduce ExceptionRuntimeNotInitialized.

---
+++ src/main/java/net/sf/kerner/utils/exception/ExceptionRuntimeNotInitialized.java
@@ -0,0 +1,32 @@
+package net.sf.kerner.utils.exception;
+
+public class ExceptionRuntimeNotInitialized extends RuntimeException {
+
+    private static final long serialVersionUID = 5014934956292054682L;
+
+    public ExceptionRuntimeNotInitialized() {
+
+    }
+
+    public ExceptionRuntimeNotInitialized(final String message) {
+        super(message);
+
+    }
+
+    public ExceptionRuntimeNotInitialized(final String message, final Throwable cause) {
+        super(message, cause);
+
+    }
+
+    public ExceptionRuntimeNotInitialized(final String message, final Throwable cause, final boolean enableSuppression,
+            final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+
+    }
+
+    public ExceptionRuntimeNotInitialized(final Throwable cause) {
+        super(cause);
+
+    }
+
+}
