49a89c957fe32a29c5cb08461b0c7a1470c6d45b
Kerner <alexanderkerner24@gmail.com>
2012-11-27 19:00:58 +0100

    Feature: Introduce ViewPoint3D*.

---
+++ src/main/java/net/sf/kerner/utils/math/point/ViewPoint3DY.java
@@ -0,0 +1,14 @@
+package net.sf.kerner.utils.math.point;
+
+import net.sf.kerner.utils.Transformer;
+
+public class ViewPoint3DY implements Transformer<Point3D, Double> {
+
+    public Double transform(final Point3D element) {
+        if (element == null) {
+            return null;
+        }
+        return element.getY();
+    }
+
+}
