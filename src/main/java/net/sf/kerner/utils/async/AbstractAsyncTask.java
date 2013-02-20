9c2508245906fcefae2d81431b75d9fbff66b861
Kerner <alexanderkerner24@gmail.com>
2013-02-20 13:28:08 +0100

    JavaDoc.

--- src/main/java/net/sf/kerner/utils/async/AbstractAsyncTask.java
+++ src/main/java/net/sf/kerner/utils/async/AbstractAsyncTask.java
@@ -3,7 +3,9 @@ package net.sf.kerner.utils.async;
 public abstract class AbstractAsyncTask<R, V> implements AsyncTask<R, V> {
 
     /**
-     * Execute this {@code AbstractAsyncCallBack}.
+     * Execute this {@code AbstractAsyncCallBack}. <br>
+     * Don't override. Override {@link #run(Object)}
+     * 
      * 
      * @param value
      *            parameter for this {@code AbstractAsyncCallBack}
