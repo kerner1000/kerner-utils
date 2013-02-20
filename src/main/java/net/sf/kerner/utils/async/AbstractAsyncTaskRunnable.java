516a916c7021b086cb9391afbbf75a73fcb0cf12
Kerner <alexanderkerner24@gmail.com>
2013-02-20 13:32:26 +0100

    JavaDoc.

--- src/main/java/net/sf/kerner/utils/async/AbstractAsyncTaskRunnable.java
+++ src/main/java/net/sf/kerner/utils/async/AbstractAsyncTaskRunnable.java
@@ -1,20 +1,21 @@
 package net.sf.kerner.utils.async;
 
+public abstract class AbstractAsyncTaskRunnable<R, V> extends AbstractAsyncTask<R, V> implements Runnable {
 
-public abstract class AbstractAsyncTaskRunnable<R, V> extends
-		AbstractAsyncTask<R, V> implements Runnable {
+    protected V value;
 
-	protected V value;
+    public synchronized V getValue() {
+        return value;
+    }
 
-	public synchronized V getValue() {
-		return value;
-	}
+    /**
+     * Don't override. Override {@link #run(Object)}
+     */
+    public void run() {
+        execute(getValue());
+    }
 
-	public void run() {
-		execute(getValue());
-	}
-
-	public synchronized void setValue(final V value) {
-		this.value = value;
-	}
+    public synchronized void setValue(final V value) {
+        this.value = value;
+    }
 }
