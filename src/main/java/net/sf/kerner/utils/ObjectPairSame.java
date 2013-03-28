f706fea99b1abc2253338ef1fe1878cd6fec2c8a
Kerner <alexanderkerner24@gmail.com>
2013-03-28 14:38:44 +0100

    ObjectPairSame deprecated. Use net.sf.kerner.utils. instead.

--- src/main/java/net/sf/kerner/utils/ObjectPairSame.java
+++ src/main/java/net/sf/kerner/utils/ObjectPairSame.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * 
+ @deprecated use {@link net.sf.kerner.uitls.ObjectPairSame} instead
+ */
+@Deprecated
 public class ObjectPairSame<T> extends ObjectPair<T, T> implements Iterable<T> {
 
     public ObjectPairSame() {
