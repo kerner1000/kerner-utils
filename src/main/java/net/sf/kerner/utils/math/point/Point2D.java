24761301029da7a805a5846d5eb3a7269d1b24bc
Kerner <alexanderkerner24@gmail.com>
2013-05-14 09:57:01 +0200

    Feature: Point2D implements ObjectPair.

--- src/main/java/net/sf/kerner/utils/math/point/Point2D.java
+++ src/main/java/net/sf/kerner/utils/math/point/Point2D.java
@@ -1,19 +1,15 @@
 package net.sf.kerner.utils.math.point;
 
-public class Point2D {
+import net.sf.kerner.utils.ObjectPair;
 
-    protected final double x;
+public class Point2D implements ObjectPair<Double, Double> {
 
-    protected final double y;
-
-    public Point2D(double x, double y) {
-        super();
-        this.x = x;
-        this.y = y;
+    final public static double getDeltaX(final Point2D one, final Point2D two) {
+        return two.x - one.x;
     }
 
-    public Point2D(Point2D template) {
-        this(template.getX(), template.getY());
+    final public static double getDeltaY(final Point2D one, final Point2D two) {
+        return two.y - one.y;
     }
 
     /**
@@ -25,30 +21,76 @@ public class Point2D {
      *            second {@code Point}
      * @return the slope
      */
-    final public static double getSlope(Point2D one, Point2D tow) {
+    final public static double getSlope(final Point2D one, final Point2D tow) {
         final double b = getDeltaX(one, tow);
         if (b == 0)
             return 0;
         return getDeltaY(one, tow) / (b);
     }
 
-    final public static double getDeltaY(Point2D one, Point2D two) {
-        return two.y - one.y;
+    protected final double x;
+
+    protected final double y;
+
+    public Point2D(final double x, final double y) {
+        super();
+        this.x = x;
+        this.y = y;
     }
 
-    final public static double getDeltaX(Point2D one, Point2D two) {
-        return two.x - one.x;
+    public Point2D(final Point2D template) {
+        this(template.getX(), template.getY());
     }
 
-    final public double getDistance(Point2D two) {
+    @Override
+    public Point2D clone() {
+        return new Point2D(getX(), getY());
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Point2D)) {
+            return false;
+        }
+        final Point2D other = (Point2D) obj;
+        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y)) {
+            return false;
+        }
+        return true;
+    }
+
+    final public double getDistance(final Point2D two) {
         return Math.sqrt(Math.pow(getDeltaX(this, two), 2) + Math.pow(getDeltaY(this, two), 2));
     }
 
-    final public double getSlope(Point2D two) {
+    public Double getFirst() {
+        return getX();
+    }
+
+    public Double getSecond() {
+        return getY();
+    }
+
+    final public double getSlope(final Point2D two) {
         return getSlope(this, two);
     }
 
-    // Override //
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 
     @Override
     public int hashCode() {
@@ -62,40 +104,13 @@ public class Point2D {
         return result;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!(obj instanceof Point2D)) {
-            return false;
-        }
-        Point2D other = (Point2D) obj;
-        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) {
-            return false;
-        }
-        if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y)) {
-            return false;
-        }
-        return true;
+    public ObjectPair<Double, Double> invert() {
+        return new Point2D(getY(), getX());
     }
 
     @Override
     public String toString() {
         return "x=" + getX() + ",y=" + getY();
-    }
-
-    // Getter / Setter //
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
     }
 
 }
