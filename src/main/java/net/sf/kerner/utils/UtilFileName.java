645146796504b246a8c7739168ead380369ebe99
Alexander Kerner <ak@silico-sciences.com>
2015-10-29 20:03:32 +0100

    API-Break: UtilFileName: change parameter from File to String. JavaDoc.

--- src/main/java/net/sf/kerner/utils/UtilFileName.java
+++ src/main/java/net/sf/kerner/utils/UtilFileName.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2010-2014 Alexander Kerner. All rights reserved.
- * 
+ * Copyright (c) 2010-2015 Alexander Kerner. All rights reserved.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,98 +18,132 @@ package net.sf.kerner.utils;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+/**
+ *
+ * Utility class for file name operations.
+ *
+ * <p>
+ * <b>Example:</b><br>
+ * </p>
+ *
+ * <p>
+ *
+ * <pre>
+ * TODO example
+ * </pre>
+ *
+ * </p>
+ *
+ * <p>
+ * <b>Threading:</b> Not thread safe.
+ * </p>
+ *
+ * <p>
+ * last reviewed: 0000-00-00
+ * </p>
+ *
+ * @author <a href="mailto:alexander.kerner@silico-sciences.com">Alexander
+ *         Kerner</a>
+ *
+ */
 public class UtilFileName {
 
-    private UtilFileName() {
-    }
+	public static File appendToFileName(File file, String string) {
+		final String raw = getRawFileName(file);
+		final String nameNew = new StringBuilder().append(raw).append(string).append(getFileExtension(file.getName()))
+				.toString();
+		final File result = new File(file.getParent(), nameNew);
+		return result;
+	}
 
-    /**
-     * <p>
-     * Renames a file
-     * <p>
-     * 
-     * @param file
-     *            file that is renamed
-     * @param newName
-     *            new name for the file
-     * @param keepExtension
-     *            if true, file extension will be appended to new name, if there
-     *            was any.
-     * @return true, if operation was successful, false otherwise
-     * @throws FileNotFoundException
-     */
-    public static boolean renameFile(File file, String newName, boolean keepExtension)
-            throws FileNotFoundException {
-        synchronized (file) {
+	/**
+	 *
+	 * @param fileName
+	 *            file name
+	 * @return the file name's extension, e.g. 'txt' or 'mp3' or an zero-length
+	 *         string if there is no file name extension
+	 */
+	public static String getFileExtension(String fileName) {
+		final int posOfExt = fileName.lastIndexOf(".");
+		if (posOfExt < 0) {
+			return "";
+		}
+		final String result = fileName.substring(posOfExt, fileName.length());
+		return result;
+	}
 
-            if (!file.isFile()) {
-                throw new FileNotFoundException("cannot access file \"" + file + "\"");
-            }
+	/**
+	 * <p>
+	 * Given a {@code File} named "hans.txt". newName is "peter". Returning
+	 * {@code String} will be "peter.txt".
+	 * </p>
+	 * <p>
+	 * Original name is "hans.txt.tex". newName is "peter". Returning
+	 * {@code String} will be "peter.tex".
+	 * </p>
+	 * <p>
+	 * Original name is "hans.txt.tex". newName is "peter.txt". Returning
+	 * {@code String} will be "peter.txt.tex".
+	 * </p>
+	 *
+	 * @param file
+	 *            {@code File}, for which new name is wanted.
+	 * @param newName
+	 *            the new filename discarding extension.
+	 * @return the new filename including extension.
+	 */
+	public static String getNewFileName(File file, String newName) {
+		final String fileName = file.getName();
+		final int posOfExt = fileName.lastIndexOf(".");
+		if (posOfExt < 0) {
+			return newName;
+		}
+		final String ext = fileName.substring(posOfExt, fileName.length());
+		return newName + ext;
+	}
 
-            final String fileNameExtension = getFileExtension(file);
+	public static String getRawFileName(File file) {
+		final String fileName = file.getName();
+		final int posOfExt = fileName.lastIndexOf(".");
+		if (posOfExt < 0) {
+			return fileName;
+		}
+		return fileName.substring(0, posOfExt);
+	}
 
-            if (keepExtension) {
-                return file.renameTo(new File(file.getParentFile(), newName + fileNameExtension));
-            } else {
-                return file.renameTo(new File(file.getParentFile(), newName));
-            }
-        }
-    }
+	/**
+	 * <p>
+	 * Renames a file
+	 * <p>
+	 *
+	 * @param file
+	 *            file that is renamed
+	 * @param newName
+	 *            new name for the file
+	 * @param keepExtension
+	 *            if true, file extension will be appended to new name, if there
+	 *            was any.
+	 * @return true, if operation was successful, false otherwise
+	 * @throws FileNotFoundException
+	 */
+	public static boolean renameFile(File file, String newName, boolean keepExtension) throws FileNotFoundException {
+		synchronized (file) {
 
-    /**
-     * <p>
-     * Given a {@code File} named "hans.txt". newName is "peter". Returning
-     * {@code String} will be "peter.txt".
-     * </p>
-     * <p>
-     * Original name is "hans.txt.tex". newName is "peter". Returning
-     * {@code String} will be "peter.tex".
-     * </p>
-     * <p>
-     * Original name is "hans.txt.tex". newName is "peter.txt". Returning
-     * {@code String} will be "peter.txt.tex".
-     * </p>
-     * 
-     * @param file
-     *            {@code File}, for which new name is wanted.
-     * @param newName
-     *            the new filename discarding extension.
-     * @return the new filename including extension.
-     */
-    public static String getNewFileName(File file, String newName) {
-        final String fileName = file.getName();
-        final int posOfExt = fileName.lastIndexOf(".");
-        if (posOfExt < 0) {
-            return newName;
-        }
-        final String ext = fileName.substring(posOfExt, fileName.length());
-        return newName + ext;
-    }
+			if (!file.isFile()) {
+				throw new FileNotFoundException("cannot access file \"" + file + "\"");
+			}
 
-    public static File appendToFileName(File file, String string) {
-        final String raw = getRawFileName(file);
-        final String nameNew = new StringBuilder().append(raw).append(string)
-                .append(getFileExtension(file)).toString();
-        final File result = new File(file.getParent(), nameNew);
-        return result;
-    }
+			final String fileNameExtension = getFileExtension(file.getName());
 
-    public static String getRawFileName(File file) {
-        final String fileName = file.getName();
-        final int posOfExt = fileName.lastIndexOf(".");
-        if (posOfExt < 0) {
-            return fileName;
-        }
-        return fileName.substring(0, posOfExt);
-    }
+			if (keepExtension) {
+				return file.renameTo(new File(file.getParentFile(), newName + fileNameExtension));
+			} else {
+				return file.renameTo(new File(file.getParentFile(), newName));
+			}
+		}
+	}
 
-    public static String getFileExtension(File file) {
-        final String fileName = file.getName();
-        final int posOfExt = fileName.lastIndexOf(".");
-        if (posOfExt < 0) {
-            return "";
-        }
-        return fileName.substring(posOfExt, fileName.length());
-    }
+	private UtilFileName() {
+	}
 
 }
