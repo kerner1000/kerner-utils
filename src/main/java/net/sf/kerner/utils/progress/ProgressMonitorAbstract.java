67e6c79e22a4d725a949ddc8372d4e3d139f2dda
Alexander Kerner <ak@silico-sciences.com>
2015-12-03 15:30:56 +0100

    Stuff.

--- src/main/java/net/sf/kerner/utils/progress/ProgressMonitorAbstract.java
+++ src/main/java/net/sf/kerner/utils/progress/ProgressMonitorAbstract.java
@@ -1,5 +1,6 @@
-/*******************************************************************************
- * Copyright (c) 2010-2014 Alexander Kerner. All rights reserved.
+/**
+ * *****************************************************************************
+ * Copyright (c) 2010-2015 Alexander Kerner. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +13,49 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ *****************************************************************************
+ */
 package net.sf.kerner.utils.progress;
 
-public abstract class ProgressMonitorAbstract implements ProgressMonitor {
+public class ProgressMonitorAbstract implements ProgressMonitor {
 
     private boolean cancelled;
+
+    private String taskName;
+
+    protected int totalWorkload = UNKNOWN_WORKLOAD;
+
+    protected int worked;
+
+    public void worked() {
+        worked(1);
+    }
+
+    
+    public void finished() {
+        totalWorkload = UNKNOWN_WORKLOAD;
+    }
+
+    
+    public void notifySubtask(String name) {
+
+    }
+
+    public void worked(int i) {
+        worked += i;
+    }
+
+    public void started(int totalWorkload) {
+        this.totalWorkload = totalWorkload;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
 
     public ProgressMonitorAbstract() {
 
